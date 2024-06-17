ee51250e0dd5a9d843d970424a18e60fe1ed5a60
MINA <mina@minaqwq.dev>
2024-06-18 01:52:26 +0200

    simpler test tool output

--- test/test.go
+++ test/test.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"crypto/tls"
-	"crypto/x509"
 	"encoding/json"
-	"fmt"
 	"log"
 	"os"
 	"strconv"
@@ -24,21 +22,16 @@ func main() {
 	if err != nil {
 		log.Fatalf("server: loadkeys: %s", err)
 	}
+
 	config := tls.Config{Certificates: []tls.Certificate{cert}, InsecureSkipVerify: true}
 	conn, err := tls.Dial("tcp", "127.0.0.1:15100", &config)
 	if err != nil {
 		log.Fatalf("client: dial: %s", err)
 	}
-	defer conn.Close()
-	log.Println("client: connected to: ", conn.RemoteAddr())
 
-	state := conn.ConnectionState()
-	for _, v := range state.PeerCertificates {
-		fmt.Println(x509.MarshalPKIXPublicKey(v.PublicKey))
-		fmt.Println(v.Subject)
-	}
-	log.Println("client: handshake: ", state.HandshakeComplete)
-	log.Println("client: mutual: ", state.NegotiatedProtocolIsMutual)
+	defer conn.Close()
+
+	log.Println("client: connected to: ", conn.RemoteAddr())
 
 	req := AuthmRequest{}
 	req.Op, err = strconv.Atoi(os.Args[1])
@@ -57,5 +50,4 @@ func main() {
 	reply := make([]byte, 256)
 	_, err = conn.Read(reply)
 	log.Print("ANSW: " + string(reply))
-	log.Print("client: exiting")
 }
