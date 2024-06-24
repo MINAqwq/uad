6297490c96308c1585ff9912b64e7cab3e52037e
MINA <mina@minaqwq.dev>
2024-06-24 23:10:39 +0200

    better build script
    build script will now move the binary into a build directory relative to
    where it got executed.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 
+GO=go
+BIN=uad
+BUILD_ARGS=-v
+
 if [ -z $1 ]; then
     echo "$0 [source_dir]"
     exit 1
 fi
 
 cd $1;
-go build -v .
-cd -;
+echo "[BUILD] $BIN"
+$GO build $BUILD_ARGS .
+cd -
+
+if [ -z $1/$BIN ]; then
+	echo "[BUILD] build failed!"
+	exit 1
+fi
+
+mkdir -v -p build
+mv -v $1/$BIN build
+
